c0d22555f1dfae4236ce8fc40e215b6110485807
Ramon <ramonp233@gmail.com>
2014-09-20 23:16:09 +0300

    Ei tea miks ei kuva tagse, commitin vahepeal.

---
+++ controllers/tags.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ramon
+ * Date: 20.09.2014
+ * Time: 22:44
+ */
+
+class tags extends Controller{
+    function index(){
+        $this->tags = get_all("SELECT tag_name, COUNT(post_id) AS count FROM post_tags NATURAL JOIN tag GROUP BY tag_id");
+    }
+}
---
+++ views/tags/tags_view.php
@@ -0,0 +1,5 @@
+<ul class="list-group">
+
+        <li class="list-group-item"><a href="tags/view/<?= $tag['tag_name'] ?>"><?=$tag['tag_name']?></a></li>
+
+</ul>
